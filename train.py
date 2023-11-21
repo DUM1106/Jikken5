37f130fed033008e16dd8aeb4c130c12698765d8
KenshinNishikawa <ticaltlcopan@protonmail.com>
2023-11-21 17:42:43 +0900

    debug train

--- train.py
+++ train.py
@@ -226,8 +226,9 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
           global_step=global_step, 
           images=image_dict,
           scalars=scalar_dict)
-
-      if global_step % hps.train.eval_interval == 0:
+    print(global_step)
+    print(hps.train.eval_interval)
+    if global_step % hps.train.eval_interval == 0:
         evaluate(hps, net_g, eval_loader, writer_eval)
         utils.save_checkpoint(net_g, optim_g, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "G_{}.pth".format(global_step)))
         utils.save_checkpoint(net_d, optim_d, hps.train.learning_rate, epoch, os.path.join(hps.model_dir, "D_{}.pth".format(global_step)))
